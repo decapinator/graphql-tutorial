9d640d8be059936af1ac30fc1baba0fdf556d99b
AnonymousLB <edmond.bounasr@isae.edu.lb>
2021-07-01 13:31:55 +0300

    graphql-tutorial

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const { mainCards, animals, categories } = require("./db");
+const typeDefs = require("./schema");
+const Query = require("./resolvers/Query");
+const Animal = require("./resolvers/Animal");
+const Category = require("./resolvers/Category");
+const Mutation = require("./resolvers/Mutation");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Animal,
+    Category,
+    Mutation,
+  },
+  context: {
+    mainCards,
+    animals,
+    categories,
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
